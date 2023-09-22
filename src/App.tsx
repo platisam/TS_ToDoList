6ad1386b17261b0a3ae0519110d21628680fc1da
platisam <platisam@gmail.com>
2023-09-22 14:06:27 +0200

    creating handleChange function for controlled inputs

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState } from "react";
+import React, { FC, ChangeEvent, useState } from "react";
 import "./App.css";
 
 const App: FC = () => {
@@ -6,12 +6,30 @@ const App: FC = () => {
   const [deadline, setDeadline] = useState<number>(0);
   const [todo, setTodo] = useState([]);
 
+  const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    if (event.target.name === "task") {
+      setTask(event.target.value);
+    } else {
+      setDeadline(Number(event.target.value));
+    }
+  };
+
   return (
     <div className="App">
       <div className="header">
         <div className="inputContainer">
-          <input type="text" placeholder="Task..." />
-          <input type="number" placeholder="Deadline (in Days)..." />
+          <input
+            type="text"
+            placeholder="Task..."
+            name="task"
+            onChange={handleChange}
+          />
+          <input
+            type="number"
+            placeholder="Deadline (in Days)..."
+            name="deadline"
+            onChange={handleChange}
+          />
         </div>
         <button>Add Task</button>
       </div>
