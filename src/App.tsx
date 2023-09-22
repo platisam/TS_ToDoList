613e11f3b223738b1446b5798494544846f06394
platisam <platisam@gmail.com>
2023-09-22 13:48:31 +0200

    setting states for inputs and button to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { FC, useState } from "react";
 import "./App.css";
 
-function App() {
-  return <div className="App">TO-Do List</div>;
-}
+const App: FC = () => {
+  const [task, setTask] = useState<string>("");
+  const [deadline, setDeadline] = useState<number>(0);
+  const [todo, setTodo] = useState([]);
+
+  return (
+    <div className="App">
+      <div className="header">
+        <div className="inputContainer">
+          <input type="text" placeholder="Task..." />
+          <input type="number" placeholder="Deadline (in Days)..." />
+        </div>
+        <button>Add Task</button>
+      </div>
+      <div className="todoList"></div>
+    </div>
+  );
+};
 
 export default App;
